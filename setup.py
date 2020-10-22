62c84d630bf579bcdd81d7930db8ed8162e350e7
Luca Mattiazzi <l.d.mattiazzi@gmail.com>
2020-10-22 20:04:17 +0200

    stuff

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="luca-jupyter-tools",
+    name="grokked_jupyter_tools",
     version="0.0.1",
     author="Luca Mattiazzi",
-    author_email="l.d.mattiazzi@gmail..com",
+    author_email="l.d.mattiazzi@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
