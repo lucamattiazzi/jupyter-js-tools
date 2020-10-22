cb5841f03ca93bed4de35b07df148dbdb6d77802
Luca Mattiazzi <l.d.mattiazzi@gmail.com>
2020-10-22 20:48:46 +0200

    stuff

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="grokked_jupyter_tools",
-    version="0.0.1",
+    version="0.0.4",
     author="Luca Mattiazzi",
     author_email="l.d.mattiazzi@gmail.com",
     description="A small example package",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lucamattiazzi/jupyter-tools",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
