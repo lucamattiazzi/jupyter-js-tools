b757853dd68c4d4863e085ba7e2f2025162b462e
Luca Mattiazzi <l.d.mattiazzi@gmail.com>
2020-10-30 18:13:49 +0100

    allnew

--- README.md
+++ README.md
@@ -4,8 +4,25 @@ A small collection of useful (?) tools written in js to use in a jupyter noteboo
 
 ## TicTacToe
 
-Play against your own A.I. player with this simple package. It requires a python function that receives the list of values in the grid and returns which will be the next move for the A.I.
+Play tic-tac-toe! The two players can be either both humans, one human and a function or two functions; it will show a tic-tac-toe game grid under the notebook cell.
 
-## Carbon
+### How does it work
 
-Plot your stuff with IBM carbon charts cool dataviz library!
+Import `tic_tac_toe` from `jupyter_js_tools` and call the function `tic_tac_toe.play_game`. This function will accep 3 parameters: the first two must be two `handlers`, the third one defines the side of the grid (default = 3).
+
+An `handler` can be one of two things: `None` if you want to let an human play, or a function that accepts the grid as an array of `['X', 'O', 0]` and the player symbol and returns the index of the next move to be played, or -1 if there is no move to be made.
+
+### Code example
+
+```python
+from jupyter_js_tools import tic_tac_toe
+import random
+
+def random_player(match, symbol):
+    free_cells = [idx for idx in range(len(match)) if match[idx] == 0]
+    if len(free_cells) == 0:
+        return -1
+    return random.choice(free_cells)
+
+tic_tac_toe.play_game('human', random_player)
+```
--- jupyter_js_tools/tic_tac_toe/index.js
+++ jupyter_js_tools/tic_tac_toe/index.js
@@ -1,52 +1,181 @@
-const { abs, sqrt, floor, sign } = Math
+const { abs, sqrt, floor, sign, random } = Math
+
+const CONTAINER = 'container'
+const GRID = 'grid'
+
+function reverseDict(dict) {
+  return Object.fromEntries(Object.entries(dict).map((v) => v.reverse()))
+}
+
+function waitToDraw() {
+  return new Promise((resolve) => setTimeout(resolve, 100))
+}
+
+function parseSide(value) {
+  const number = Number(value)
+  if (isNaN(number) || number < 2) return 3
+  return number
+}
+
+function getFunctionName(player) {
+  return `tic_tac_toe.player_${player.toLowerCase()}_fn`
+}
+
+const HUMAN_ACTION = 'human'
+const FN_ACTION = 'function'
 
 class Match {
-  constructor(identifier) {
-    this.grid = Array(9).fill(0)
+  currentPlayer = 'X'
+  playerValue = {
+    X: 1,
+    O: -1,
+  }
+  playerActions = {
+    X: HUMAN_ACTION,
+    O: HUMAN_ACTION,
+  }
+
+  constructor() {
+    this.container = document.getElementById(CONTAINER)
+    this.setSide()
+      .then(this.setPlayers)
+      .then(this.setFirstPlayer)
+      .then(this.generateTitle)
+      .then(this.generateGrid)
+      .then(this.generateButton)
+      .then(this.start)
+  }
+
+  get currentPlayerAction() {
+    return this.playerActions[this.currentPlayer]
+  }
+
+  get currentPlayerValue() {
+    return this.playerValue[this.currentPlayer]
+  }
+
+  get valuePlayer() {
+    return { ...reverseDict(this.playerValue), 0: 0 }
+  }
+
+  get gridForPython() {
+    return this.grid.map((value) => this.valuePlayer[value])
+  }
+
+  setPlayers = async () => {
+    const promises = Object.keys(this.playerValue).map(this.setPlayer)
+    return Promise.all(promises)
+  }
+
+  setPlayer = async (player) => {
+    const fnName = getFunctionName(player)
+    return executePython(fnName).then((fn) => {
+      this.playerActions[player] = fn === 'human' || fn === 'None' ? HUMAN_ACTION : FN_ACTION
+    })
+  }
+
+  setFirstPlayer = () => {
+    this.currentPlayer = random() > 0.5 ? 'X' : 'O'
+  }
+
+  generateTitle = () => {
+    this.title = document.createElement('div')
+    this.title.id = 'title'
+    this.setTitle()
+    this.container.appendChild(this.title)
+  }
+
+  generateButton = () => {
+    this.button = document.createElement('button')
+    this.button.id = 'button'
+    this.button.onclick = () => this.restart()
+    this.button.innerText = 'RESTART'
+    this.container.appendChild(this.button)
+  }
+
+  setTitle = () => {
+    this.title.innerText = `Current player turn: ${this.currentPlayer}`
+  }
+
+  generateGrid = () => {
+    this.grid = Array(this.side ** 2).fill(0)
+    this.gridContainer = document.createElement('div')
+    this.gridContainer.id = 'grid'
+    this.container.appendChild(this.gridContainer)
+    this.gridContainer.style.width = `${this.side * 100}px`
     this.gridDom = this.grid.map((_, idx) => {
       const cell = document.createElement('div')
       cell.className = 'ttt-cell'
       cell.innerText = '-'
-      cell.onclick = () => this.handleClick(idx)
+      cell.onclick = this.handleClick(idx)
       return cell
     })
-    this.container = document.getElementById(identifier)
     for (const cell of this.gridDom) {
-      this.container.appendChild(cell)
+      this.gridContainer.appendChild(cell)
     }
   }
 
-  get side() {
-    return sqrt(this.grid.length)
+  start = () => {
+    this.reset(true)
+    this.runTurn()
   }
 
-  reset = () => {
+  restart = () => {
+    this.reset()
+    this.runTurn()
+  }
+
+  setSide = () => {
+    return executePython('tic_tac_toe.grid_size').then((side) => {
+      this.side = parseSide(side)
+    })
+  }
+
+  updateCurrentPlayer = () => {
+    this.currentPlayer = Object.keys(this.playerValue).filter((v) => v !== this.currentPlayer)[0]
+  }
+
+  nextTurn = () => {
+    const someoneHasWon = this.checkWin()
+    if (someoneHasWon) return
+    this.updateCurrentPlayer()
+    this.setTitle()
+    this.runTurn()
+  }
+
+  runTurn = () => {
+    if (this.currentPlayerAction === HUMAN_ACTION) return
+    const functionName = getFunctionName(this.currentPlayer)
+    const pythonCode = `${functionName}(${JSON.stringify(this.gridForPython)}, ${
+      this.currentPlayer
+    })`
+    executePython(pythonCode)
+      .then((idx) => {
+        const parsedIdx = Number(idx)
+        if (parsedIdx < 0) return this.reset()
+        return this.setGridCell(parsedIdx)
+      })
+      .then(this.nextTurn)
+  }
+
+  reset = (firstTime = false) => {
     for (const idx in this.grid) {
       this.grid[idx] = 0
       this.gridDom[idx].innerText = '-'
     }
+    if (!firstTime) this.setFirstPlayer()
   }
 
-  restartGame = () => {
-    alert('Game over!')
-    this.reset()
+  handleClick = (idx) => () => {
+    if (this.currentPlayerAction !== HUMAN_ACTION) return
+    if (this.grid[idx] !== 0) return
+    this.setGridCell(idx).then(this.nextTurn)
   }
 
-  handleClick = (idx) => {
-    if (this.grid[idx] !== 0) return alert('Cell already used!')
-    this.grid[idx] = 1
-    this.gridDom[idx].innerText = 'X'
-    const over = this.checkWin()
-    if (over) return
-    executePython(`player_fn(${JSON.stringify(this.grid)})`)
-      .then((jdx) => {
-        if (jdx === '-1') return this.restartGame()
-        this.grid[jdx] = -1
-        this.gridDom[jdx].innerText = 'O'
-        return new Promise((resolve) => setTimeout(resolve, 100))
-      })
-      .then(this.checkWin)
-      .catch((err) => console.log(err))
+  setGridCell = (idx) => {
+    this.grid[idx] = this.currentPlayerValue
+    this.gridDom[idx].innerText = this.currentPlayer
+    return waitToDraw()
   }
 
   checkForWinner = (group) => {
@@ -58,8 +187,7 @@ class Match {
   }
 
   closeGame = (winner) => {
-    alert(`${winner} is the winner!`)
-    this.restartGame()
+    this.title.innerText = `${winner} is the winner!`
     return true
   }
 
@@ -114,4 +242,4 @@ function executePython(python) {
   })
 }
 
-new Match('grid')
+new Match()
